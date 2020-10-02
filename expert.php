476b5710b17c10f3f5f270d401e03a582ee819f5
Beardificent <cis.magito@gmail.com>
2020-10-02 10:34:19 +0200

    fixed the function so that it updates the ex number

--- expert.php
+++ expert.php
@@ -7,19 +7,29 @@ ini_set("display_startup_errors", '1');
 error_reporting(E_ALL);
 
 //HAD TO DECLARE $X and CALL UPON FUNCTION
-function newExercise() {
-    $x = 1;
+function newExercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 
 }
-newExercise();
-//HAD TO DECLARE $X and CALL UPON FUNCTION
-newExercise();
+newExercise(2);
+//INSTEAD OF DEFINING X I USED IT AS A PARAMETER and CALL UPON FUNCTION
+newExercise(3);
 //Arrays start at 0 not at 1
 $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 $monday = $week[0];
 
 echo $monday;
 
-newExercise();
+newExercise(4);
+
+$str = '"Debugged !"';
+echo substr($str, 0, 15);
+
+newExercise(5);
+
+foreach($week as $day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
